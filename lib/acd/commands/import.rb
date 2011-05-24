0ab3a1c13b484e9075d51908ef41cb175dd089db
Jason Felice <jason.m.felice@gmail.com>
2011-05-24 13:48:16 -0400

    Fix system() calls

--- lib/acd/commands/import.rb
+++ lib/acd/commands/import.rb
@@ -17,9 +17,9 @@ module ACD
       end
 
       def invoke
-        abort "Unable to add git submodule" unless system(*["git", "submodule", "add", origin, "submodules/#{name}"])
-        abort "Unable to add files" unless system(*["git", "add", ".gitmodules", "submodules/#{name}"])
-        abort "Unable to commit" unless system(*["git", "commit", "-m", "Import #{name}"])
+        abort "Unable to add git submodule" unless system "git", "submodule", "add", origin, "submodules/#{name}"
+        abort "Unable to add files" unless system "git", "add", ".gitmodules", "submodules/#{name}"
+        abort "Unable to commit" unless system "git", "commit", "-m", "Import #{name}"
       end
       
     end
